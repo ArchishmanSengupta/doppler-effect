e506ebdb72ee3393248d3a0f96f2c8d3b3693f74
Archishman Sengupta <71402528+ArchishmanSengupta@users.noreply.github.com>
2021-08-10 20:13:02 +0530

    bootstrap login using spotify

--- client/src/Login.js
+++ client/src/Login.js
@@ -1,14 +1,19 @@
 import React from 'react'
 
+// RENDER out client access using a single container from bootstrap library
+
+import { Container } from 'react-bootstrap'
+
 
 // https://developer.spotify.com/documentation/general/guides/authorization-guide/
 
 const AUTH_URL="https://accounts.spotify.com/authorize?client_id=c33f91ccfc09457eabda45fc6a8458cb&response_type=code&redirect_uri=http://localhost:3000&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state"
 
 export default function Login() {
-    return (
-        <div>
-            <h1> testing </h1>
-        </div>
+    return( <Container>
+        <a className="btn btn-success btn-lg" href={AUTH_URL}>
+            Login With Spotify
+        </a>
+    </Container>
     )
 }
